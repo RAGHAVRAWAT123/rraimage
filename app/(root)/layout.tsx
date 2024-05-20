0862c72dbf9ff02b31b7105addf5ccc9471b0e57
RAGHAVRAWAT123 <raghavrawat290@gmail.com>
2024-05-20 16:33:17 +0530

    implement clerk webhooks

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,22 @@
 import MobileNav from '@/components/shared/Mobilenav'
 import Sidebar from '@/components/shared/sidebar'
-import React from 'react'
+import { Toaster } from '@/components/ui/toaster'   
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <main className="root">
-            <Sidebar />
-            <MobileNav />
-            <div className="root-conatiner">
-                <div className="wrapper">
-                </div>
-            </div>
-            {children}
-        </main>
-    )
+  return (
+    <main className="root">
+      <Sidebar />
+      <MobileNav />
+
+      <div className="root-container">
+        <div className="wrapper">
+          {children}
+        </div>
+      </div>
+      
+      <Toaster />
+    </main>
+  )
 }
 
 export default Layout
